1991ac337b8f5c01865511ed28bee911c38f45ef
Gonzagant <76914278+Gonzagant@users.noreply.github.com>
2021-02-02 16:12:54 -0300

    commit

--- Linguagens_de_Programacao/SQL/Estudo de Oracle DB.sql
+++ Linguagens_de_Programacao/SQL/Estudo de Oracle DB.sql
@@ -22,3 +22,22 @@ arquivos do tipo .dbf (data base file)
 */
 Drop Table NOME_DA_TABELA;
 
+/* Normalmento no Oracle, criamos um Objeto apontado para uma estrutura Lógica e essa para uma 
+estrutura Física - OLF
+
+Existem Tablespaces permanentes e temporárias
+
+Multiplexação - Controlfile - fornece informações para Start UP e Shut Down
+os processos executados fazem Check Points nos Controlfiles e também guardam informações 
+sobre qual tablespace aponta para um arquivo tipo 01.dbf e 02.dbf
+
+Redo Log Files - armazenam os commits
+CTL diz o que já ta em memoria e o que está em banco
+esses Redos são informações sobre se o arquivo .dbf já processou a alteração, ou se está na 
+memória isso para que se caso o BD caia, seja possível recuperar as informações
+
+Existe um modo chamado Archive que contém vários Redo Log Files para ir atualizando 
+conforme a demanda
+
+
+
