eb3a89f92712aeac8e42213eb6c9370f1096572b
Gonzagant <76914278+Gonzagant@users.noreply.github.com>
2021-02-09 00:09:53 -0300

    commit

--- Linguagens de Programação/C++/det5x5_V2.cpp
+++ Linguagens de Programação/C++/det5x5_V2.cpp
@@ -1,26 +1,51 @@
-//Calculadora de Determinante 5x5 refatorada
+//Calculadora de Determinante nxn refatorada
 #include <iostream>
 using namespace std;
 int main() {
 
 //Entrada da ordem da matriz
-int n;
-cout<<"insira o numero da ordem da matriz";
-cin>>n;
+int ordem;
+cout<<"insira o numero da ordem da matriz"<<endl;
+cin>>ordem;
+while (ordem<0){
+    cout<<"A ordem da matriz não pode ser um número negativo";
+    cin>>ordem;
+}
+cout<<endl<<endl;
 
 //Entrada de dados da matriz nxn
-int i, j, matriz[n][n];
-for(i=0; i<n; i++){
-    for(j=0; j<n; j++){
-        cout<<"insira o elemento da linha "<<i+1<<" e coluna "<<j+1<<"\n";
+int i, j, matriz[ordem][ordem];
+cout<<"insira os elementos da matriz"<<endl<<endl;
+for(i=0; i<ordem; i++){
+    for(j=0; j<ordem; j++){
         cin>>matriz[i][j];
     }
 }
-//Lógica do cálculo de determinante (Teorema de Gauss-Jordan)
-double resultado;
 
-//Mostra a matriz completa e o determinante na linha de baixo
+//Mostra a matriz completa
+cout<<endl<<"Matriz de ordem "<<ordem<<" inserida:"<<endl<<endl;
+for(i=0; i<ordem; i++){
+    for(j=0; j<ordem; j++){
+        cout<<matriz[i][j];
+        cout<<'\t';
+    }
+    cout<<endl;
+}
+cout<<endl<<endl;
 
+//Lógica de triangulação da matriz
+double pivo;
+pivo = matriz[i][i]/matriz[i][j];
+
+//Lógica do cálculo de determinante
+double resultado=1;
+for(i=0; i<ordem; i++){
+    for(j=0; j<ordem; j++){
+        resultado = resultado * matriz[i][i];
+    }
+}
+
+//Mostra o determinante da matriz
 cout<<"O Determinante dessa matriz e': "<<resultado;
 
 //Fim         
