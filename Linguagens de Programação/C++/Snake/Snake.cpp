84d74e4c18356e5fe23c565dc4ca110a8e789f4f
Gonzagant <76914278+Gonzagant@users.noreply.github.com>
2021-02-22 18:59:12 -0300

    commit

--- Linguagens de Programação/C++/Snake/Snake.cpp
+++ Linguagens de Programação/C++/Snake/Snake.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
+#include <conio.h>
+
 using namespace std;
+
 bool gameOver;
 const int width = 20;
 const int height = 20;
 int x, y, fruitX, fruitY, score;
+int tailX[100], tailY[100];
+int nTail;
 enum eDirection {STOP = 0, LEFT, RIGHT, UP, DOWN};
 eDirection dir;
 
+//Configuração inicial
 void Setup()
 {
     gameOver = false;
@@ -18,11 +24,12 @@ void Setup()
     score = 0;
 }
 
+//Impressão na tela
 void Draw()
 {
     system("cls"); // system("clear")
     for (int i = 0; i < width + 2; i++)
-        cout << "*";
+        cout << "#";
     cout << endl;
 
     for (int i = 0; i < height + 2; i++)
@@ -30,34 +37,121 @@ void Draw()
         for (int j = 0; j < width; j++)
         {
             if (j == 0)
-                cout << "*";
+                cout << "#";
             if (i == y && j == x)
                 cout << "O";
             else if (i == fruitY && j == fruitX)
                 cout << "a";
-            else 
+            else
+            {
+                bool print = false;
+                for (int k = 0; k < nTail; k++)
+                {                    
+                    if (tailX[k] == j && tailY[k] == i)
+                    {
+                        cout << "o";
+                        print = true;
+                    }
+                }
+                if (!print)
                 cout << " ";
-            
+            }           
             if (j == width - 1)
-                cout << "*";
+                cout << "#";
         }
         cout << endl;
     }
     for (int i = 0; i < width + 2; i++)
-        cout << "*";
+        cout << "#";
     cout << endl;
+    cout << "Score:" << score << endl;
 }
 
+//Entradas pelo teclado
 void Input()
 {
-    
+   if (_kbhit())
+   {
+       switch (_getch())
+       {
+        case 'a':
+            dir = LEFT;
+            break;
+        case 'd':
+            dir = RIGHT;
+            break;
+        case 'w':
+            dir = UP;
+            break;
+        case 's':
+            dir = DOWN;
+            break;
+        case 'c':
+            gameOver = true;
+            break; 
+       }
+   } 
 }
 
+//Lógica do jogo
 void Logic()
 {
-    
+    //Corpo da Cobra
+    int prevX = tailX[0];
+    int prevY = tailY[0];
+    int prev2X, prev2Y;
+    tailX[0] = x;
+    tailY[0] = y;
+    for (int i = 1; i < nTail; i++)
+    {
+        prev2X = tailX[i];
+        prev2Y = tailY[i];
+        tailX[i] = prevX;
+        tailY[i] = prevY;
+        prevX = prev2X;
+        prevY = prev2Y;        
+    }
+    //Controle da Cobra
+    switch (dir)
+    {
+    case STOP:
+        break;
+    case LEFT:
+        x--;
+        break;
+    case RIGHT:
+        x++;
+        break;
+    case UP:
+        y--;
+        break;
+    case DOWN:
+        y++;
+        break;
+    default:
+        break;  
+    }
+    //Bater na parede = Game Over
+    if (x > width || x < 0 || y > height || y < 0)
+        gameOver = true;
+    //Bater na parede e sair do outro lado
+    /*if (x >= width) x = 0; else if (x < 0) x = width - 1;
+    if (y >= height) y = 0; else if (y < 0) y = height - 1;*/
+    //Bater no próprio corpo
+    for (int i = 0; i < nTail; i++)
+        if (tailX[i] == x && tailY[i] == y)
+        gameOver = true;
+    //Pontuação e posição da fruta
+    if (x == fruitX && y == fruitY)
+    {
+        score += 10;
+        fruitX = rand() % width;
+        fruitY = rand() % height;
+        nTail++;
+    }
 }
 
+//Função principal que roda tudo
 int main()
 {
     Setup();
