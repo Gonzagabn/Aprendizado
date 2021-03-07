bee614b2379cf8418aae9f8195e3ff7ea691a3ea
Gonzagant <76914278+Gonzagant@users.noreply.github.com>
2021-03-07 10:33:10 -0300

    commit

--- Linguagens de Programação/Dart/Exercicio.dart
+++ Linguagens de Programação/Dart/Exercicio.dart
@@ -1,1 +1,22 @@
-main() {}
+void main() {
+  var conjunto = {0, 1, 2, 3, 4};
+  print(conjunto.length);
+
+  var notasDosAlunos = <String, double>{
+    'Ana': 9.3,
+    'Bia': 7.7,
+    'Carlos': 5.5,
+  };
+  //Mostra chave
+  for (var chave in notasDosAlunos.keys) {
+    print('chave = $chave');
+  }
+  //Mostra valor
+  for (var valor in notasDosAlunos.values) {
+    print('valor = $valor');
+  }
+  //Mostra chave e valor
+  for (var registro in notasDosAlunos.entries) {
+    print('${registro.key} = ${registro.value}');
+  }
+}
