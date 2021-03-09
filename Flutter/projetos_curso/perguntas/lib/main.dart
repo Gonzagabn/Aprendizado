e71a9287cc1334ba99c02c6baade554bcea20e86
Gonzagant <76914278+Gonzagant@users.noreply.github.com>
2021-03-08 22:35:42 -0300

    commit

--- Flutter/projetos_curso/perguntas/lib/main.dart
+++ Flutter/projetos_curso/perguntas/lib/main.dart
@@ -1,51 +1,56 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import './questao.dart';
+import 'questionario.dart';
+import 'resultado.dart';
 
 main() => runApp(
     PerguntaApp()); // O componente PerguntaApp que foi passado para o runApp é a raiz de toda a arvore de componentes
 
 //A classe PerguntaApp se torna widget porque herdanda as características da classe StateLessWidget
 class _PerguntaAppState extends State<PerguntaApp> {
-  var _perguntaSelecionada = 0;
+  int _perguntaSelecionada = 0;
+  final _perguntas = const [
+    {
+      'texto': 'Qual é a sua cor favorita?',
+      'respostas': ['Preto', 'Azul', 'Cinza', 'Branco'],
+    },
+    {
+      'texto': 'Qual é o seu animal favorito?',
+      'respostas': ['Águia', 'Pinguin', 'Tigre', 'Capivara'],
+    },
+    {
+      'texto': 'Qual é o seu professor favorito?',
+      'respostas': ['Léo', 'Akita', 'Jacob', 'Filipe'],
+    }
+  ];
 
   void _responder() {
-    setState(() {
-      _perguntaSelecionada++;
-    });
-    print(_perguntaSelecionada);
+    if (temPerguntaSelecionada) {
+      setState(() {
+        _perguntaSelecionada++;
+      });
+    }
+  }
+
+  bool get temPerguntaSelecionada {
+    return _perguntaSelecionada < _perguntas.length;
   }
 
   @override //O @override é uma anotação que sobrescreve o método build da classe mãe StateLessWidget
   Widget build(BuildContext context) {
-    final perguntas = [
-      'Qual é a sua cor favorita?',
-      'Qual é o seu animal favorito',
-    ];
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Perguntas'),
-        ),
-        body: Column(
-          children: <Widget>[
-            Questao(perguntas[_perguntaSelecionada]),
-            ElevatedButton(
-              child: Text('Resposta 1'),
-              onPressed: _responder,
-            ),
-            ElevatedButton(
-              child: Text('Resposta 2'),
-              onPressed: _responder,
-            ),
-            ElevatedButton(
-              child: Text('Resposta 3'),
-              onPressed: _responder,
-            ),
-          ],
-        ),
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('Perguntas'),
       ),
-    );
+      body: temPerguntaSelecionada
+          ? Questionario(
+              perguntas: _perguntas,
+              perguntaSelecionada: _perguntaSelecionada,
+              quandoResponder: _responder,
+            )
+          : Resultado(),
+    ));
   }
 }
 
