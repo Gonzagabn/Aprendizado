b91caa1a18c5149bce620b045534552636485918
Gonzagant <76914278+Gonzagant@users.noreply.github.com>
2021-04-09 19:50:13 -0300

    commit

---
+++ Linguagens de Programação/Dart/Curso 2 - Dart para Flutter/04 - Orientação a Objetos/03_classe_metodo_construtor.dart
@@ -0,0 +1,81 @@
+//Cada classe só pode ter um único construtor padrão
+
+class Data {
+  int? dia;
+  int? mes;
+  int? ano;
+  //O método construtor é o que permite criar objetos a partir de uma classe
+  //Aqui o construtor "Data()" está disponível normalmente
+}
+
+class Data2 {
+  int? dia;
+  int? mes;
+  int? ano;
+  //Por padrão o método construtor é implicito com essa característica:
+  Data2() {} //Em toda classe esse construtor fica escondido e é o padrão
+}
+
+class Data3 {
+  int? dia;
+  int? mes;
+  int? ano;
+  //O construtor pode receber os parâmetros da classe de forma explicita:
+  Data3(int dia, int mes, int ano) {} //Agora esse é o construtor padrão
+  //O construtor padrão "Data3()" não existe mais
+  //Os parâmetros passam a ser obrigatórios
+}
+
+class Data4 {
+  int? dia;
+  int? mes;
+  int? ano;
+  //Agora com um construtor que define os valores de dia, mes e ano:
+  Data4(int diaInicial, int mesInicial, int anoInicial) {
+    dia = diaInicial;
+    /*Aqui é definido que o atributo "dia" da classe será setado com o mesmo
+    parâmetro "diaInicial" do construtor*/
+    mes = mesInicial;
+    ano = anoInicial;
+  }
+}
+
+//Formato melhor e Padrão:
+class Data5 {
+  int? dia;
+  int? mes;
+  int? ano;
+  Data5(int dia, int mes, int ano) {
+    this.dia = dia; //O "this." vai acessar o atributo do objeto "data5"
+    this.mes = mes; //Sem o "this." o atributo do objeto não será definido
+    ano = ano; //Sem o "this." o atributo retorna "null"
+  }
+}
+
+main() {
+  var data1 = new Data(); //"Data()" é o método construtor da classe
+  data1.dia = 10;
+  data1.mes = 3;
+  data1.ano = 1992;
+  print("data1 é: ${data1.dia}/${data1.mes}/${data1.ano}");
+  //imprime: data1 é: 10/3/1992
+
+  var data2 = new Data2(); //"Data2()" funciona normalmento como o "Data()"
+  data2.dia = 10;
+  data2.mes = 3;
+  data2.ano = 1992;
+  print("data2 é: ${data2.dia}/${data2.mes}/${data2.ano}");
+  //imprime: data2 é: 10/3/1992
+
+  var data3 = new Data3(10, 3, 1992); //"Data3()" tem parâmetros obrigatórios
+  print("data3 é: ${data3.dia}/${data3.mes}/${data3.ano}");
+  //imprime: data3 é: null/null/null
+
+  var data4 = new Data4(10, 3, 1992); //"Data4()" tem parâmetros obrigatórios
+  print("data4 é: ${data4.dia}/${data4.mes}/${data4.ano}");
+  //imprime: data4 é: 10/3/1992
+
+  var data5 = new Data5(10, 3, 1992); //"Data5()" tem parâmetros obrigatórios
+  print("data5 é: ${data5.dia}/${data5.mes}/${data5.ano}");
+  //imprime: data5 é: 10/3/null
+}
