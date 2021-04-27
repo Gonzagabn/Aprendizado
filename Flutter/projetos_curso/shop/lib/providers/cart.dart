4685408c8564e8b501ae1cd005d58288485dff65
Gonzaga <luizgonzagabn@gmail.com>
2021-04-27 11:19:32 -0300

    commit

--- Flutter/projetos_curso/shop/lib/providers/cart.dart
+++ Flutter/projetos_curso/shop/lib/providers/cart.dart
@@ -5,12 +5,14 @@ import 'package:shop/providers/product.dart';
 
 class CartItem {
   final String? id;
+  final String? productId;
   final String? title;
   final int? quantity;
   final double? price;
 
   CartItem({
     @required this.id,
+    @required this.productId,
     @required this.title,
     @required this.quantity,
     @required this.price,
@@ -41,6 +43,7 @@ class Cart with ChangeNotifier {
       _items.update(product.id!, (existingItem) {
         return CartItem(
           id: existingItem.id,
+          productId: product.id,
           title: existingItem.title,
           quantity: existingItem.quantity! + 1,
           price: existingItem.price,
@@ -51,12 +54,18 @@ class Cart with ChangeNotifier {
         product.id!,
         () => CartItem(
           id: Random().nextDouble().toString(),
+          productId: product.id,
           title: product.title,
           quantity: 1,
           price: product.price,
         ),
       );
     }
+    notifyListeners();
+  }
+
+  void removeItem(String productId) {
+    _items.remove(productId);
     notifyListeners();
   }
 }
--- Flutter/projetos_curso/shop/lib/widgets/cart_item_widget.dart
+++ Flutter/projetos_curso/shop/lib/widgets/cart_item_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shop/providers/cart.dart';
 
 class CartItemWidget extends StatelessWidget {
@@ -8,23 +9,46 @@ class CartItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.symmetric(
-        horizontal: 15,
-        vertical: 4,
+    return Dismissible(
+      key: ValueKey(cartItem.id),
+      background: Container(
+        color: Theme.of(context).errorColor,
+        child: Icon(
+          Icons.delete,
+          color: Colors.white,
+          size: 40,
+        ),
+        alignment: Alignment.centerRight,
+        padding: EdgeInsets.only(right: 20),
+        margin: EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 4,
+        ),
       ),
-      child: Padding(
-        padding: EdgeInsets.all(8),
-        child: ListTile(
-          leading: CircleAvatar(
-            child: Padding(
-              padding: EdgeInsets.all(5),
-              child: FittedBox(child: Text('${cartItem.price}')),
+      direction: DismissDirection.endToStart,
+      onDismissed: (_) {
+        Provider.of<Cart>(context, listen: false)
+            .removeItem(cartItem.productId!);
+      },
+      child: Card(
+        margin: EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 4,
+        ),
+        child: Padding(
+          padding: EdgeInsets.all(8),
+          child: ListTile(
+            leading: CircleAvatar(
+              child: Padding(
+                padding: EdgeInsets.all(5),
+                child: FittedBox(child: Text('${cartItem.price}')),
+              ),
             ),
+            title: Text(cartItem.title!),
+            subtitle:
+                Text('Total: R\$ ${cartItem.price! * cartItem.quantity!}'),
+            trailing: Text('${cartItem.quantity}x'),
           ),
-          title: Text(cartItem.title!),
-          subtitle: Text('Total: R\$ ${cartItem.price! * cartItem.quantity!}'),
-          trailing: Text('${cartItem.quantity}x'),
         ),
       ),
     );
