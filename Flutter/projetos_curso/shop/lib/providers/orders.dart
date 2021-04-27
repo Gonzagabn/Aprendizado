94667cd1b4db953121a124c6e56c1645898050da
Gonzaga <luizgonzagabn@gmail.com>
2021-04-27 11:46:57 -0300

    commit

--- Flutter/projetos_curso/shop/lib/providers/orders.dart
+++ Flutter/projetos_curso/shop/lib/providers/orders.dart
@@ -24,14 +24,14 @@ class Orders with ChangeNotifier {
     return [..._orders];
   }
 
-  void addOrder(List<CartItem> products, double total) {
+  void addOrder(Cart cart) {
     _orders.insert(
       0,
       Order(
         id: Random().nextDouble().toString(),
-        total: total,
+        total: cart.totalAmount,
         date: DateTime.now(),
-        products: products,
+        products: cart.items.values.toList(),
       ),
     );
     notifyListeners();
--- Flutter/projetos_curso/shop/lib/views/cart_screen.dart
+++ Flutter/projetos_curso/shop/lib/views/cart_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop/providers/cart.dart';
+import 'package:shop/providers/orders.dart';
 import 'package:shop/widgets/cart_item_widget.dart';
 
 class CartScreen extends StatelessWidget {
@@ -40,7 +41,12 @@ class CartScreen extends StatelessWidget {
                   ),
                   Spacer(),
                   TextButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      Provider.of<Orders>(context, listen: false).addOrder(
+                        cart,
+                      );
+                      cart.clear();
+                    },
                     child: Text('COMPRAR'),
                   ),
                 ],
