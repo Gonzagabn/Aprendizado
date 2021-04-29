3faf8fa878718f828754da1d2d76fe4cd372d3ef
Gonzaga <luizgonzagabn@gmail.com>
2021-04-29 17:01:16 -0300

    commit

--- Flutter/projetos_curso/shop/lib/views/product_form_screen.dart
+++ Flutter/projetos_curso/shop/lib/views/product_form_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:shop/providers/product.dart';
 
 class ProductFormScreen extends StatefulWidget {
   @override
@@ -10,6 +13,8 @@ class _ProductFormScreenState extends State<ProductFormScreen> {
   final _descriptionFocusNode = FocusNode();
   final _imageUrlFocusNode = FocusNode();
   final _imageUrlController = TextEditingController();
+  final _form = GlobalKey<FormState>();
+  final _formData = Map<String, Object>();
 
   @override
   void initState() {
@@ -30,16 +35,44 @@ class _ProductFormScreenState extends State<ProductFormScreen> {
     _imageUrlFocusNode.dispose();
   }
 
+  void _saveForm() {
+    var isValid = _form.currentState!.validate();
+
+    if (!isValid) {
+      return;
+    }
+
+    _form.currentState!.save();
+
+    final newProduct = Product(
+      id: Random().nextDouble().toString(),
+      title: _formData['title'] as String,
+      description: _formData['description'] as String,
+      price: _formData['price'] as double,
+      imageUrl: _formData['imageUrl'] as String,
+    );
+    print(newProduct);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Formulário Produto'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.save),
+            onPressed: () {
+              _saveForm();
+            },
+          )
+        ],
         centerTitle: true,
       ),
       body: Padding(
         padding: const EdgeInsets.all(15.0),
         child: Form(
+          key: _form,
           child: ListView(
             children: <Widget>[
               TextFormField(
@@ -47,6 +80,18 @@ class _ProductFormScreenState extends State<ProductFormScreen> {
                 textInputAction: TextInputAction.next,
                 onFieldSubmitted: (_) {
                   FocusScope.of(context).requestFocus(_priceFocusNode);
+                },
+                onSaved: (value) => _formData['title'] = value!,
+                validator: (value) {
+                  if (value!.trim().isEmpty) {
+                    return 'Informe um título válido!';
+                  }
+
+                  if (value.trim().length >= 3) {
+                    return 'Informe um título com no mínimo 3 letras!';
+                  }
+
+                  return null;
                 },
               ),
               TextFormField(
@@ -56,6 +101,7 @@ class _ProductFormScreenState extends State<ProductFormScreen> {
                 keyboardType: TextInputType.numberWithOptions(
                   decimal: true,
                 ),
+                onSaved: (value) => _formData['price'] = double.parse(value!),
                 onFieldSubmitted: (_) {
                   FocusScope.of(context).requestFocus(_descriptionFocusNode);
                 },
@@ -65,6 +111,7 @@ class _ProductFormScreenState extends State<ProductFormScreen> {
                 maxLines: 3,
                 keyboardType: TextInputType.multiline,
                 //textInputAction: TextInputAction.next,(only IOS) (Refatorar)
+                onSaved: (value) => _formData['description'] = value!,
                 focusNode: _descriptionFocusNode,
               ),
               Row(
@@ -77,6 +124,10 @@ class _ProductFormScreenState extends State<ProductFormScreen> {
                       textInputAction: TextInputAction.done,
                       focusNode: _imageUrlFocusNode,
                       controller: _imageUrlController,
+                      onSaved: (value) => _formData['imageUrl'] = value!,
+                      onFieldSubmitted: (_) {
+                        _saveForm();
+                      },
                     ),
                   ),
                   Container(
