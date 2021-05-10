552b220bfb16409e446e776cd9da96108d2a3442
Gonzaga <luizgonzagabn@gmail.com>
2021-05-10 18:29:31 -0300

    commit

--- Flutter/projetos_curso/shop/lib/main.dart
+++ Flutter/projetos_curso/shop/lib/main.dart
@@ -6,6 +6,7 @@ import 'package:shop/providers/orders.dart';
 import 'package:shop/providers/products.dart';
 
 import 'package:shop/utils/app_routes.dart';
+import 'package:shop/views/auth_screen.dart';
 
 import 'package:shop/views/cart_screen.dart';
 import 'package:shop/views/product_detail_screen.dart';
@@ -39,6 +40,7 @@ class MyApp extends StatelessWidget {
           fontFamily: 'Lato',
         ),
         routes: {
+          AppRoutes.AUTH: (ctx) => AuthScreen(),
           AppRoutes.HOME: (ctx) => ProductOverviewScreen(),
           AppRoutes.PRODUCT_DETAIL: (ctx) => ProductDetailScreen(),
           AppRoutes.CART: (ctx) => CartScreen(),
---
+++ Flutter/projetos_curso/shop/lib/views/auth_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class AuthScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color.fromRGBO(215, 117, 255, 0.5),
+                  Color.fromRGBO(255, 188, 117, 0.9),
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            child: Column(
+              children: <Widget>[
+                Container(
+                  child: Text(
+                    'Minha Loja',
+                    style: TextStyle(
+                      color: Theme.of(context).accentTextTheme.headline6.color,
+                      fontSize: 45,
+                      fontFamily: 'Anton',
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
